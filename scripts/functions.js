4525a3363a2923f0dfa049381875e78f12eaf1fd
GrRabby <grrabby9@gmail.com>
2026-02-23 01:34:26 +0600

    added animation effects

--- scripts/functions.js
+++ scripts/functions.js
@@ -13,19 +13,22 @@ function add_job_cards(jobs_list){
     document.getElementById('availabe-jobs').innerText =  `${filtered_jobs.length} of ${jobs_list.length} Jobs`;
     job_section.innerHTML = ''
     // iterate all jobs and create the cards for each job and append
+    let index = 0
     for(jobs of filtered_jobs){
         // hide the default no jobs card
         document.getElementById('no-jobs-card').hidden = true
         const div = document.createElement('div');
         div.setAttribute('id', 'cards');
-        div.setAttribute('class', 'bg-base-100 p-6 rounded-lg border border-base-300 shadow-md');
+        div.setAttribute('class', 'bg-base-100 p-6 rounded-lg border border-base-300 shadow-md border-l-4 hover:-translate-y-0.5 transition duration-400 ease-in-out opacity-0 -translate-x-10');
+        if(jobs.status === 'INTERVIEW') div.classList.add('border-l-green-600/50')
+        if(jobs.status === 'REJECTED') div.classList.add('border-l-red-600/50')
         div.innerHTML = `
             <div class="w-full space-y-1 flex justify-between items-center">
                 <div>
                     <h1 class="text-[18px] font-semibold">${jobs.company}</h1>
                     <h2 class="text-4 text-[#64748B]">${jobs.designation}</h2>
                 </div>
-                <button class="btn btn-soft h-8 w-8 rounded-[1000px] bg-transparent flex" onclick="openDeleteModal(${jobs.JobId})"><i class="fa-regular fa-trash-can"></i></button>
+                <button class="btn btn-soft btn-primary h-8 w-8 rounded-[1000px] flex border" onclick="openDeleteModal(${jobs.JobId})"><i class="fa-regular fa-trash-can"></i></button>
             </div>
             <p class="text-[14px] text-gray-600 my-5">${jobs.salary}</p>
             <div id="badge" class="bg-gray-200 w-[113px] h-9 flex justify-center items-center py-2 px-3 mb-2 rounded-sm">
@@ -33,10 +36,10 @@ function add_job_cards(jobs_list){
             </div>
             <p class="text-base-900 text-[14px]">${jobs.details}</p>
             <div id="buttons" class="mt-5 space-x-2">
-                <button class="btn btn-soft border border-[#10B981] bg-base-100 max-h-9" onclick="update_job_status(${jobs.JobId},'INTERVIEW')">
+                <button class="btn btn-soft border border-[#10B981] bg-base-100 max-h-9" onclick="update_job_status(${jobs.JobId},'INTERVIEW',this)">
                     <p class="text-[#10B981] font-bold">Interview</p>
                 </button>
-                <button class="btn btn-soft border border-[#EF4444] bg-base-100 max-h-9" onclick="update_job_status(${jobs.JobId},'REJECTED')">
+                <button class="btn btn-soft border border-[#EF4444] bg-base-100 max-h-9" onclick="update_job_status(${jobs.JobId},'REJECTED',this)">
                     <p class="text-[#EF4444] font-bold">Rejected</p>
                 </button>
             </div>
@@ -55,6 +58,11 @@ function add_job_cards(jobs_list){
         
         
         job_section.appendChild(div);
+        setTimeout(() => {
+            div.classList.remove('opacity-0', '-translate-x-10');
+            div.classList.add('opacity-100',);
+        }, 10);
+        index++
     }
 }
 function tabs_switched(element,type){
@@ -86,7 +94,7 @@ function remove_job(id){
         update_job_status(id) // reset the status to default
     }
 }
-function update_job_status(id,type='NOT APPLIED'){
+function update_job_status(id,type='NOT APPLIED',element=null){
     for(jobs of jobs_data){
         if(jobs.JobId === id){
             if(type === 'REJECTED'){
